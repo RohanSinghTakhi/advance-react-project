d051d226ebd9c1705b886d04d5a33b778935d84a
RohanSinghTakhi <co23545@ccet.ac.in>
2024-07-15 22:10:23 +0530

    contact form done

---
+++ contact_form/src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+
+import './App.css'
+import Navigation from './components/Navigation'
+import ContactHeader from './components/ContactHeader/ContactHeader'
+
+function App() {
+
+
+  return (
+    <>
+      <Navigation>
+
+      </Navigation>
+      <ContactHeader></ContactHeader>
+    </>
+  )
+}
+
+export default App
---
+++ contact_form/src/components/Navigation.jsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+function Navigation() {
+  return (
+   <nav className='container'>
+    <div className='Logo '>
+        <h2>Logo</h2>
+    </div>
+
+    <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Contact</li>
+    </ul>
+   </nav>
+
+  )
+}
+
+export default Navigation
